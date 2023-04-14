a49a7bd01f8bb8dddd8c2ffbdda30d8a28bba7c4
senku <senku.kz@gmail.com>
2023-04-14 19:50:10 +0600

    PascalsTriangle

---
+++ PascalsTriangle.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        return new ArrayList<>();
+    }
+
+    public static void main (String[] arg) {
+        PascalsTriangle obj = new PascalsTriangle();
+
+        System.out.println(obj.generate(5));
+        System.out.println(obj.generate(1));
+    }
+}
