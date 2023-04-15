1eadb21c40e4453c9685f2d7c1dddf572ffc6c87
senku <senku.kz@gmail.com>
2023-04-15 14:38:06 +0600

    Two Sum II - Input array is sorted

---
+++ ArrayAndString/TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,24 @@
+package ArrayAndString;
+
+import java.util.Arrays;
+
+/*
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Your solution must use only constant extra space.
+*/
+
+public class TwoSum2InputArrayIsSorted {
+    public int[] twoSum(int[] numbers, int target) {
+        return numbers;
+    }
+
+    public static void main (String[] arg) {
+        TwoSum2InputArrayIsSorted obj = new TwoSum2InputArrayIsSorted();
+
+        System.out.println(Arrays.toString(obj.twoSum(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(obj.twoSum(new int[]{2,3,4}, 6)));
+        System.out.println(Arrays.toString(obj.twoSum(new int[]{-1,0}, -1)));
+    }
+}
