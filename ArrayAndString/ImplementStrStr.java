262a005406beeb4d196f2895b0bb350e9d42a658
senku <senku.kz@gmail.com>
2023-04-15 13:48:56 +0600

    Implement strStr()

---
+++ ArrayAndString/ImplementStrStr.java
@@ -0,0 +1,14 @@
+package ArrayAndString;
+
+public class ImplementStrStr {
+    public int strStr(String haystack, String needle) {
+        return -1;
+    }
+
+    public static void main (String[] arg) {
+        ImplementStrStr obj = new ImplementStrStr();
+
+        System.out.println(obj.strStr("sadbutsad", "sad"));
+        System.out.println(obj.strStr("leetcode", "leeto"));
+    }
+}
