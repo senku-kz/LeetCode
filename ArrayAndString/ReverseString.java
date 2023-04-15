718aca82d057abaa715e10435a20427fadf8a4c8
senku <senku.kz@gmail.com>
2023-04-15 14:16:44 +0600

    Reverse String

--- ArrayAndString/ReverseString.java
+++ ArrayAndString/ReverseString.java
@@ -4,7 +4,14 @@ import java.util.Arrays;
 
 public class ReverseString {
     public void reverseString(char[] s) {
-
+        char c;
+        int i = 0, j = s.length-1;
+        while (i<j){
+            c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+            i++; j--;
+        }
     }
 
     public static void main (String[] arg) {
