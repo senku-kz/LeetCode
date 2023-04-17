147a1f9c166a744072fa6655870974f3766c0c06
senku <senku.kz@gmail.com>
2023-04-17 17:18:46 +0600

    RemoveElement Two Sum II - Input array is sorted

---
+++ ArrayAndString/RemoveElementTwoPointerTechnique.java
@@ -0,0 +1,33 @@
+package ArrayAndString;
+
+import java.util.Arrays;
+
+public class RemoveElementTwoPointerTechnique {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    public static void main (String[] arg) {
+        RemoveElementTwoPointerTechnique obj = new RemoveElementTwoPointerTechnique();
+
+        int[] t1 = new int[]{2,7,2,11,15};
+        System.out.println(obj.removeElement(t1, 2));
+        System.out.println(Arrays.toString(t1));
+
+        int[] t2 = new int[]{2,3,4};
+        System.out.println(obj.removeElement(t2, 2));
+        System.out.println(Arrays.toString(t2));
+
+        int[] t3 = new int[]{-1,0,5,6,0,5,6,0,0,4};
+        System.out.println(obj.removeElement(t3, 0));
+        System.out.println(Arrays.toString(t3));
+
+    }
+}
