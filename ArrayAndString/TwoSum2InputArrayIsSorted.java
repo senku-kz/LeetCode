257a60c2f4b11bb87951488efeaf631f8028595d
senku <senku.kz@gmail.com>
2023-04-17 17:07:54 +0600

    Two Sum II - Input array is sorted

--- ArrayAndString/TwoSum2InputArrayIsSorted.java
+++ ArrayAndString/TwoSum2InputArrayIsSorted.java
@@ -11,7 +11,14 @@ Your solution must use only constant extra space.
 
 public class TwoSum2InputArrayIsSorted {
     public int[] twoSum(int[] numbers, int target) {
-        return numbers;
+        for (int i=0; i<numbers.length-1; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                if((numbers[i] + numbers[j]) == target){
+                    return new int[]{i+1,j+1};
+                }
+            }
+        }
+        return new int[2];
     }
 
     public static void main (String[] arg) {
