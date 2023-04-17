d6573c464b5f103b6c298546bdffacca0e2abf50
senku <senku.kz@gmail.com>
2023-04-17 18:06:07 +0600

    Minimum Size Subarray Sum

--- ArrayAndString/MinimumSizeSubarraySum.java
+++ ArrayAndString/MinimumSizeSubarraySum.java
@@ -1,7 +1,5 @@
 package ArrayAndString;
 
-import java.util.Arrays;
-
 /*
 Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
  */
