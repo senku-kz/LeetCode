ec56dc524e6ca41dca9cb816254b666072bf16ab
senku <senku.kz@gmail.com>
2023-04-18 17:52:00 +0600

    Minimum Size Subarray Sum

--- ArrayAndString/MinimumSizeSubarraySum.java
+++ ArrayAndString/MinimumSizeSubarraySum.java
@@ -5,39 +5,38 @@ Given an array of positive integers nums and a positive integer target, return t
  */
 public class MinimumSizeSubarraySum {
     public int minSubArrayLen(int target, int[] nums) {
-        int min = nums.length;
-        boolean k = false;
-        for (int i=0; i<nums.length; i++){
-            if (nums[i]==target) return 1;
+        int l = 0, sum = 0;
+        int res = nums.length + 1;
+        for (int r=0; r<nums.length; r++) {
+            if (nums[r] == target) return 1;
 
-            int sum = nums[i], cnt = 1;
-            for (int j=i+1; j<nums.length; j++){
-                sum += nums[j];
-                cnt++;
-                if (sum==target && min>=cnt) {
-                    min = cnt;
-                    k = true;
+            sum += nums[r];
+
+            while (sum >= target) {
+                if ((r - l + 1) < res) {
+                    res = r - l + 1;
                 }
-                if (sum>target) break;
+                sum = sum - nums[l];
+                l++;
             }
         }
-        return k ? min : 0;
+       return res == nums.length + 1 ? 0 : res;
     }
 
     public static void main (String[] arg) {
         MinimumSizeSubarraySum obj = new MinimumSizeSubarraySum();
 
-//        //2
-//        int[] a1 = new int[]{2,3,1,2,4,3};
-//        System.out.println(obj.minSubArrayLen(7, a1));
-//
-//        //1
-//        int[] a2 = new int[]{1,4,4};
-//        System.out.println(obj.minSubArrayLen(4, a2));
-//
-//        //0
-//        int[] a3 = new int[]{1,1,1,1,1,1,1,1};
-//        System.out.println(obj.minSubArrayLen(11, a3));
+        //2
+        int[] a1 = new int[]{2,3,1,2,4,3};
+        System.out.println(obj.minSubArrayLen(7, a1));
+
+        //1
+        int[] a2 = new int[]{1,4,4};
+        System.out.println(obj.minSubArrayLen(4, a2));
+
+        //0
+        int[] a3 = new int[]{1,1,1,1,1,1,1,1};
+        System.out.println(obj.minSubArrayLen(11, a3));
 
         //3
         int[] a4 = new int[]{1,2,3,4,5};
